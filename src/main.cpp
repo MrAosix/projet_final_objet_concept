0f195895848533c75b197b128491aa91ce068826
Émeryk Bélisle <emeryk.belisle@gmail.com>
2025-12-02 10:19:45 -0500

    first try

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,28 @@
 #include <FastLED.h>
 #include <VL53L0X.h>
 
-VL53L0X  tof1;
 M5_PbHub myPbHub;
-#define channelAngle 0
+
+VL53L0X  tof;
+#define channelAngle1 0
+#define channelAngle2 1
+#define channelAngle3 2
+#define channelAngle4 3
+#define channelAngle5 4
+#define channelAngle6 5
 #define lumiere 27
 
 CRGB pixel;
+unsigned long monChronoDepart;
 
-MicroOscSlip<128> monOsc(&Serial);
+MicroOscSlip<128> osc(&Serial);
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   myPbHub.begin();
-  tof1.init();
+  tof.init();
+
   FastLED.addLeds<WS2812, lumiere, GRB>(&pixel, 1);
 
   pixel = CRGB(255,0,0);
@@ -31,17 +39,27 @@ void setup() {
   delay(1000);
   pixel = CRGB(0,0,0);
   FastLED.show();
-
 }
 
 void loop() {
-  int maLectureAnalogique = myPbHub.analogRead(channelAngle);
-  //int mesureTof1 = tof1.readRangeSingleMillimeters();
- // int mesureTof2 = tof2.readRangeSingleMillimeters();
- int mesureTof1 = tof1.readRangeSingleMillimeters();
- // monOsc.sendInt( "/tufPitch" , mesureTof1);
- // monOsc.sendInt( "/tufVolume", mesureTof2);
-  monOsc.sendInt( "/analog" , maLectureAnalogique);
-  monOsc.sendInt( "/tof", mesureTof1);
-  delay(20);
+  if ( millis() - monChronoDepart >= 20 ) { 
+    monChronoDepart = millis(); 
+
+    int lectureAngle1 = myPbHub.analogRead(channelAngle1);
+    int lectureAngle2 = myPbHub.analogRead(channelAngle2);
+    int lectureAngle3 = myPbHub.analogRead(channelAngle3);
+    int lectureAngle4 = myPbHub.analogRead(channelAngle4);
+    int lectureAngle5 = myPbHub.analogRead(channelAngle5);
+    int lectureAngle6 = myPbHub.analogRead(channelAngle6);
+    int mesureTof = tof.readRangeSingleMillimeters();
+
+    osc.sendInt( "/angleVolume", lectureAngle1);
+    osc.sendInt( "/angleChorus" , lectureAngle2);
+    osc.sendInt( "/angleReverb", lectureAngle3);
+    osc.sendInt( "/anglePan" , lectureAngle4);
+    osc.sendInt( "/angleEffectA", lectureAngle5);
+    osc.sendInt( "/angleEffectB", lectureAngle6);
+  
+    osc.sendInt( "/tufVolume", mesureTof);
+  }
 }
